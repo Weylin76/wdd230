782d4214471c1c3d24ebec9cb0a355038e934300
Weylin76 <Weylin76@msn.com>
2024-01-19 01:08:45 -0500

    applied scale to button

--- scripts/nav.js
+++ scripts/nav.js
@@ -4,6 +4,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
    hamBtn.addEventListener('click', function() {
        nav.classList.toggle('nav-menu-close');
+        
+       // Apply scale
+        hamBtn.style.transform = 'scale(0.90)';
+
+       // Revert scale after a delay (e.g., 150ms)
+        setTimeout(function() {
+            hamBtn.style.transform = 'scale(1)';
+        }, 150);
    });
 
 });
