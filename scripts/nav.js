800b45f0f706dc89cabd4678772b59475e42958f
Weylin76 <Weylin76@msn.com>
2024-01-19 16:44:24 -0500

    added scale to buttons on hamburger menu

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,17 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
    const hamBtn = document.getElementById('menu-btn');
+   const closeBtn = document.getElementById('close-btn');
    const nav = document.querySelector('#nav');
 
    hamBtn.addEventListener('click', function() {
-       nav.classList.toggle('nav-menu-close');
-        
-       // Apply scale
-        hamBtn.style.transform = 'scale(0.90)';
+       
+      // Apply scale
+      hamBtn.style.transform = 'scale(0.90)';
 
+      setTimeout(function(){
+         hamBtn.transform = 'scale(1)';
+         nav.classList.remove('nav-menu-close');
+         closeBtn.classList.remove('close-close-btn');
+         hamBtn.classList.add('close-ham-btn');
+      }, 150);
+      
+   }); 
+
+   closeBtn.addEventListener('click', function(){
+
+      closeBtn.style.transform = 'scale(0.90)';
+      
        // Revert scale after a delay
-        setTimeout(function() {
-            hamBtn.style.transform = 'scale(1)';
-        }, 150);
-   });
+       setTimeout(function() {
+         closeBtn.style.transform = 'scale(1)';
+         nav.classList.add('nav-menu-close');
+         hamBtn.classList.remove('close-ham-btn');
+         closeBtn.classList.add('close-close-btn');
+     }, 150);
 
+   });
 });
