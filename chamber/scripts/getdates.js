fb648cfc0c9547c188107643c0ef91042e962f62
Weylin76 <Weylin76@msn.com>
2024-01-29 22:50:54 -0500

    hero img done sectons created

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,11 @@
+//create year and last modified varables
+const now = new Date();
+const currentYear = now.getFullYear();
+let dateTime = document.lastModified;
+
+// Append to footer paragraphs
+const domCurrentYear = document.querySelector('#current-year');
+domCurrentYear.innerText = currentYear;
+
+const lastModified = document.querySelector('#current-date');
+lastModified.innerText = dateTime
