452b055351ea5c8c27f546e1aa2f6e7b61339979
Weylin76 <Weylin76@msn.com>
2024-03-05 21:27:04 -0500

    removed business without pictures

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -15,17 +15,43 @@ async function loadMembers(url) {
 
         const card = document.createElement('section');
         card.className = 'card';
-
+        
+        //Create Busniess Name with H2 element
         const businessName = document.createElement('h2');
         businessName.textContent = `${business.name}`
-        console.log(business.name)
 
+        //Create Address information
+        const businessAddress = document.createElement('p');
+        businessAddress.innerHTML = `${business.address} <br> ${business.city}, ${business.state} ${business.zip}`
 
+        //Create phone
+        const businessPhone = document.createElement('p');
+        businessPhone.textContent = `${business.phone}`;
 
+        //Use set source for img and alt
+        const businessLogo = document.createElement('img');
+        businessLogo.setAttribute('src', business.image);
+        businessLogo.setAttribute('alt', business.alt);
+        businessLogo.setAttribute('loading', 'lazy');
+        // businessLogo.setAttribute('width', '100');
+        // businessLogo.setAttribute('height', '100');
 
-    
+        //Create website information
+        const businessWebsite = document.createElement('p');
+        businessWebsite.textContent = `${business.website}`
 
+        //Create membership Level
+        const businessMembershipLevel = document.createElement('p');
+        businessMembershipLevel.textContent = `Membership level: ${business.membershipLevel}`;
+        
+
+        cards.appendChild(businessLogo);
         cards.appendChild(businessName);
+        cards.appendChild(businessAddress);
+        cards.appendChild(businessPhone);
+        cards.appendChild(businessWebsite);
+        cards.appendChild(businessMembershipLevel);
+        
     })
     } catch (error) {
         console.error('Fetch error:', error);
