de1ba20e64953f43b6c083b3b5d6c73edb3f4e37
Weylin76 <Weylin76@msn.com>
2024-03-12 19:58:42 -0400

    added F if windchill does apply

--- scripts/weather.js
+++ scripts/weather.js
@@ -40,7 +40,7 @@ function calculateWindChill(temperature, speed) {
             0.6215 * temperature -
             35.75 * Math.pow(speed, 0.16) +
             0.4275 * temperature * Math.pow(speed, 0.16)
-        ).toFixed(1);
+        ).toFixed(1) + '°F';
     } else {
         return "N/A";
     }
