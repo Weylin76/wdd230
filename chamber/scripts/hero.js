ce2fb3b7c1eb0f5d62f94edf485d0e2a40fb4afd
Weylin76 <Weylin76@msn.com>
2024-03-12 20:18:25 -0400

    Fixed join button

--- chamber/scripts/hero.js
+++ chamber/scripts/hero.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
         let heroText = document.getElementById('hero');
         // Check viewport width and set text accordingly
         if (window.innerWidth <= 450) {
-            heroText.innerHTML = '<button>Join</button>';
+            heroText.innerHTML = '<a href= "join.html"><button>Join</button><a>';
             heroText.style.fontSize = '2rem';
         } else if (window.innerWidth <= 750) {
-            heroText.innerHTML = '<button>Join</button>';
+            heroText.innerHTML = '<a href= "join.html"><button>Join</button><a>';
             heroText.style.fontSize = '2rem';
         } else {
-            heroText.innerHTML = '<button>Join</button>';
+            heroText.innerHTML = '<a href= "join.html"><button>Join</button><a>';
             heroText.style.fontSize = '2rem';
         }
     }
---
+++ chamber/scripts/weather.js
@@ -0,0 +1,4 @@
+const temp = document.querySelector('#temp');
+const 
+
+api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
