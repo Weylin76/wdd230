403813a46acdc020674c90b864372f29c498a188
Weylin76 <Weylin76@msn.com>
2024-03-15 19:52:22 -0400

    fixed menu button to stay open across page change.

--- chamber/scripts/nav.js
+++ chamber/scripts/nav.js
@@ -1,32 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hamBtn = document.getElementById('menu-btn');
-    const closeBtn = document.getElementById('close-btn');
-    const nav = document.querySelector('#nav');
- 
-    hamBtn.addEventListener('click', function() {
-        
-       // Apply scale
+   const hamBtn = document.getElementById('menu-btn');
+   const closeBtn = document.getElementById('close-btn');
+   const nav = document.querySelector('#nav');
+   
+   // Check the state on page load and update menu accordingly
+   const isMenuOpen = localStorage.getItem('isMenuOpen') === 'true';
+   if (isMenuOpen) {
+       nav.classList.remove('nav-menu-close');
+       closeBtn.classList.remove('close-close-btn');
+       hamBtn.classList.add('close-ham-btn');
+   } else {
+       nav.classList.add('nav-menu-close');
+       closeBtn.classList.add('close-close-btn');
+       hamBtn.classList.remove('close-ham-btn');
+   }
+
+   hamBtn.addEventListener('click', function() {
        hamBtn.style.transform = 'scale(0.90)';
- 
+
        setTimeout(function(){
-          hamBtn.transform = 'scale(1)';
-          nav.classList.remove('nav-menu-close');
-          closeBtn.classList.remove('close-close-btn');
-          hamBtn.classList.add('close-ham-btn');
+           hamBtn.style.transform = 'scale(1)';
+           nav.classList.remove('nav-menu-close');
+           closeBtn.classList.remove('close-close-btn');
+           hamBtn.classList.add('close-ham-btn');
+           localStorage.setItem('isMenuOpen', 'true'); // Store the state
        }, 150);
-    }); 
- 
-    closeBtn.addEventListener('click', function(){
- 
+   }); 
+
+   closeBtn.addEventListener('click', function(){
        closeBtn.style.transform = 'scale(0.90)';
-       
-        // Revert scale after a delay
-        setTimeout(function() {
-          closeBtn.style.transform = 'scale(1)';
-          nav.classList.add('nav-menu-close');
-          hamBtn.classList.remove('close-ham-btn');
-          closeBtn.classList.add('close-close-btn');
-      }, 150);
-    });
- });
+      
+       setTimeout(function() {
+           closeBtn.style.transform = 'scale(1)';
+           nav.classList.add('nav-menu-close');
+           hamBtn.classList.remove('close-ham-btn');
+           closeBtn.classList.add('close-close-btn');
+           localStorage.setItem('isMenuOpen', 'false'); // Store the state
+       }, 150);
+   });
+});
 
